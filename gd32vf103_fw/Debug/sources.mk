1ce8beb04b1cb3a50e019f4b0941bf317ef9ac50
Baram <chcbaram@gmail.com>
2021-09-02 00:08:15 +0900

    폴더 정리

---
+++ gd32vf103_fw/Debug/sources.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+OBJS := 
+SECONDARY_FLASH := 
+SECONDARY_SIZE := 
+ASM_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+src/ap \
+src/bsp \
+src/bsp/device/RISCV/drivers \
+src/bsp/device/RISCV \
+src/bsp/device/RISCV/stubs \
+src/bsp/startup \
+src/common/core \
+src/hw/driver \
+src/hw \
+src/lib/GD32VF103_standard_peripheral/Source \
+src/lib/GD32VF103_standard_peripheral \
+src \
+
